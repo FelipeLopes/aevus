297031fd4dd7a8b5dfb73108ee0da4a523242155
FelipeLopes <felipelopesfreitas@gmail.com>
2022-10-30 07:06:14 -0300

    implement scaler

--- src/scaler.cpp
+++ src/scaler.cpp
@@ -1,9 +1,25 @@
 #include <gmpxx.h>
+#include "complex_type.hpp"
 #include "scaler.hpp"
 
 template <typename T>
 Scaler<T>::Scaler(T iniLogscale, T step, int numSteps, T prec) {
+    base = exp<T>(iniLogscale, prec);
+    lookup.resize(32-__builtin_clz(numSteps));
+    for (int i=0; i<lookup.size(); i++) {
+        lookup[i] = exp<T>((1<<i)*step, prec);
+    }
+}
 
+template <typename T>
+T Scaler<T>::lookupExp(int n) {
+    T ans = base;
+    while (n > 0) {
+        int bits = (n & -n);
+        ans = ans * lookup[__builtin_ctz(bits)];
+        n -= bits;
+    }
+    return ans;
 }
 
 template class Scaler<mpq_class>;
--- src/scaler.hpp
+++ src/scaler.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <vector>
+
 template <typename T>
 class Scaler {
 public:
     Scaler(T iniLogscale, T step, int numSteps, T prec);
+    T lookupExp(int n);
+private:
+    T base;
+    std::vector<T> lookup;
 };
