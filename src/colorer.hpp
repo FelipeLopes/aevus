06154ec48e35549224d7f501b0b2d57b93fea904
FelipeLopes <felipelopesfreitas@gmail.com>
2022-11-05 17:51:58 +0100

    move scaler and colorer to header

--- src/colorer.hpp
+++ src/colorer.hpp
@@ -1,17 +1,37 @@
 #pragma once
 
 #include "palette.hpp"
+#include <algorithm>
 #include <boost/gil.hpp>
 
 class Colorer {
+
+using rgb8_pixel_t = boost::gil::rgb8_pixel_t;
+
 public:
-    Colorer();
+    Colorer(): palette(WHITE, RED) {
+
+    }
     struct ColorParams {
         Palette palette;
         std::vector<double> colorValues;
     };
     ColorParams color(int numTransforms, int diveTransform, double logscale,
-        double iniKeyLogscale, double endKeyLogscale) const;
+        double iniKeyLogscale, double endKeyLogscale) const {
+
+        ColorParams params;
+        params.palette = palette;
+        double f = (logscale-iniKeyLogscale)/(endKeyLogscale-iniKeyLogscale);
+        double diveVal = std::min(1.0, 2*f);
+        double nonDiveVal = std::max(0.0, 2*f-1);
+
+        for (int i=0; i<numTransforms; i++) {
+            double val = (i == diveTransform) ? diveVal : nonDiveVal;
+            params.colorValues.push_back(val);
+        }
+
+        return params;
+    }
     Palette palette;
 private:
     static const boost::gil::rgb8_pixel_t RED, WHITE;
--- src/scaler.hpp
+++ src/scaler.hpp
@@ -2,11 +2,29 @@
 
 #include <vector>
 
+#include "complex_type.hpp"
+
 template <typename T>
 class Scaler {
 public:
-    Scaler(T iniLogscale, T step, int numSteps, T prec);
-    T lookupExp(int n) const;
+    Scaler(T iniLogscale_, T step_, int numSteps_, T prec):
+        iniLogscale(iniLogscale_), step(step_), numSteps(numSteps_) {
+
+        base = exp<T>(iniLogscale, prec);
+        lookup.resize(32-__builtin_clz(numSteps));
+        for (int i=0; i<lookup.size(); i++) {
+            lookup[i] = exp<T>((1<<i)*step, prec);
+        }
+    }
+    T lookupExp(int n) const {
+        T ans = base;
+        while (n > 0) {
+            int bits = (n & -n);
+            ans = ans * lookup[__builtin_ctz(bits)];
+            n -= bits;
+        }
+        return ans;
+    }
     const T iniLogscale, step;
     const int numSteps;
 private:
