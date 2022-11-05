8c4e167aaed6ba0409b3635ac6117398cc1d6910
FelipeLopes <felipelopesfreitas@gmail.com>
2022-11-05 17:08:47 +0100

    move diver to header only

--- src/diver.hpp
+++ src/diver.hpp
@@ -6,8 +6,19 @@
 template<typename T>
 class Diver {
 public:
-    Diver(int depth, int seed);
-    int chooseDive(Mobius<T> accumulator);
+    Diver(int depth_, int seed): depth(depth_), rng(seed),
+        dist2(0,1), dist3(0,2) {
+
+    }
+    int chooseDive(Mobius<T> acc) {
+        if (acc.a == Complex<T>(1) && acc.b == Complex<T>(0) &&
+            acc.c == Complex<T>(0) && acc.d == Complex<T>(1)) {
+
+            int k = dist2(rng);
+            return k*3 + dist3(rng);
+        }
+        return dist3(rng);
+    }
     int depth;
 private:
     std::mt19937 rng;
