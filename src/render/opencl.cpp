454e4fe472a46500d3fc1f65ef8a695d9e008f8f
FelipeLopes <felipelopesfreitas@gmail.com>
2022-11-12 10:28:38 +0100

    populate platforms and devices

--- src/render/opencl.cpp
+++ src/render/opencl.cpp
@@ -12,6 +12,28 @@ OpenCL::OpenCL() {
         auto ec = std::error_code(ret, std::generic_category());
         throw std::system_error(ec, "Could not get number of OpenCL platforms.");
     }
+    platformIDs.resize(numPlatforms);
+    deviceIDs.resize(numPlatforms);
+    ret = clGetPlatformIDs(1, platformIDs.data(), NULL);
+    if (ret != CL_SUCCESS) {
+        auto ec = std::error_code(ret, std::generic_category());
+        throw std::system_error(ec, "Could not get OpenCL platform IDs.");
+    }
+    for (int i = 0; i < numPlatforms; i++) {
+        cl_uint numDevices;
+        ret = clGetDeviceIDs(platformIDs[i], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+        if (ret != CL_SUCCESS) {
+            auto ec = std::error_code(ret, std::generic_category());
+            throw std::system_error(ec, "Could not get number of device IDs for OpenCL platform");
+        }
+        deviceIDs[i].resize(numDevices);
+        ret = clGetDeviceIDs(platformIDs[i], CL_DEVICE_TYPE_ALL, numDevices,
+            deviceIDs[i].data(), NULL);
+        if (ret != CL_SUCCESS) {
+            auto ec = std::error_code(ret, std::generic_category());
+            throw std::system_error(ec, "Could not get device IDs for OpenCL platform");
+        }
+    }
 }
 
 CLContext OpenCL::getContext() {
--- src/render/opencl.hpp
+++ src/render/opencl.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "cl_context.hpp"
+#include <CL/cl.h>
+#include <vector>
 
 namespace render {
 
@@ -15,6 +17,8 @@ private:
     OpenCL();
     OpenCL(OpenCL const&) = delete;
     void operator= (OpenCL const&) = delete;
+    std::vector<cl_platform_id> platformIDs;
+    std::vector<std::vector<cl_device_id>> deviceIDs;
 };
 
 }
