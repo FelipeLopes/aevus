d7d8c5f04e7201a50ba3104b47029c8f8d69d046
Felipe Lopes <felipelopesfreitas@gmail.com>
2026-03-06 13:37:59 -0300

    fixes and updates

--- src/core/color.hpp
+++ src/core/color.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../serial/serializable.hpp"
+#include <inttypes.h>
 
 namespace core {
 
--- wxfb/gen.sh
+++ wxfb/gen.sh
@@ -1,6 +1,6 @@
 Xvfb :99 -screen 0, 1024x768x16 2>/dev/null &
 THE_PID=$!
-DISPLAY=:99 wxformbuilder -g $1 2>/dev/null
+DISPLAY=:99 /home/felipe/wxFormBuilder/_install/bin/wxformbuilder -g $1 2>/dev/null
 mv wxfb/wxfb_frame.h $2
 mv wxfb/wxfb_frame.cpp $3
 sed -i '10i #include <wx/docview.h>' $2
