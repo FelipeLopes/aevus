4777449129102029aec40d2ff6f1b0c9b4131137
Felipe Lopes <felipelopesfreitas@gmail.com>
2026-03-27 12:40:38 -0300

    folder stub

--- src/ui/gradient_model.cpp
+++ src/ui/gradient_model.cpp
@@ -1,57 +1,129 @@
 #include "gradient_model.hpp"
-#include <wx-3.2/wx/variant.h>
+#include <wx/dataview.h>
+#include <wx/variant.h>
 #include <wx/debug.h>
 #include <wx/log.h>
 
 namespace ui {
 
 GradientModel::GradientModel() {
-    modelNode.title = "my_val";
-    modelNode.value = 67;
+    mapping = {
+        {
+            "A", {
+                {"January", 31},
+                {"February", 28},
+                {"March", 31}
+            }
+        },
+        {
+            "B", {
+                {"April", 30},
+                {"May", 31},
+                {"June", 30}
+            }
+        },
+        {
+            "C", {
+                {"July", 31},
+                {"August", 31},
+                {"September", 30}
+            }
+        },
+        {
+            "D", {
+                {"October", 31},
+                {"November", 30},
+                {"December", 31}
+            }
+        }
+    };
+    for (auto it=mapping.begin(); it != mapping.end(); it++) {
+        folders.emplace_back(std::make_unique<GradientContainerNode>(nullptr, it->first));
+        for (int i=0; i<it->second.size(); i++) {
+            folders.back()->addEntityChild(&it->second[i]);
+        }
+    }
 }
 
 bool GradientModel::IsContainer(const wxDataViewItem& item) const {
-    return !item.IsOk();
+    auto node = static_cast<GradientModelNode*>(item.GetID());
+    return (node == NULL) || (node->isContainer());
 }
 
 wxDataViewItem GradientModel::GetParent(const wxDataViewItem& item) const {
-    return wxDataViewItem(NULL);
+    auto node = static_cast<GradientModelNode*>(item.GetID());
+    if (node == NULL) {
+        return wxDataViewItem(NULL);
+    }
+    return wxDataViewItem(node->parent);
 };
 
 unsigned GradientModel::GetChildren(const wxDataViewItem& item, wxDataViewItemArray& children) const  {
     GradientModelNode* node = static_cast<GradientModelNode*>(item.GetID());
     if (node == NULL) {
-        children.Add(wxDataViewItem((void*)&modelNode));
-        return 1;
-    } else {
-        return 0;
+        for (auto& fld: folders) {
+            children.Add(wxDataViewItem(fld.get()));
+        }
+        return folders.size();
+    } else if (auto container = dynamic_cast<GradientContainerNode*>(node)){
+        for (auto& child: container->children) {
+            children.Add(wxDataViewItem(child.get()));
+        }
+        return container->children.size();
     }
+    return 0;
 }
 
 void GradientModel::GetValue(wxVariant& variant, const wxDataViewItem& item, unsigned col) const {
     wxASSERT(item.IsOk());
     GradientModelNode* node = static_cast<GradientModelNode*>(item.GetID());
-    switch (col) {
-        case 0: variant = node->title; break;
-        case 1: variant = (long)node->value; break;
-        default: wxLogError("GradientModel::GetValue wrong column %d", col);
-    } 
+    if (auto container = dynamic_cast<GradientContainerNode*>(node)) {
+        switch (col) {
+            case 0: variant = container->name; break;
+            case 1: /* do nothing */ break;
+            default: wxLogError("GradientModel::GetValue wrong column %d", col);
+        }
+    } else if (auto leaf = dynamic_cast<GradientEntityNode*>(node)) {
+        switch (col) {
+            case 0: variant = leaf->entity->title; break;
+            case 1: variant = (long)leaf->entity->value; break;
+            default: wxLogError("GradientModel::GetValue wrong column %d", col);
+        }
+    } else {
+        wxLogError("GradientModel::GetValue unknown GradientModelNode");
+    }
 }
 
 bool GradientModel::SetValue(const wxVariant& variant, const wxDataViewItem& item, unsigned col) {
     wxASSERT(item.IsOk());
     GradientModelNode* node = static_cast<GradientModelNode*>(item.GetID());
-    switch (col) {
-        case 0:
-            node->title = variant.GetString();
-            return true;
-        case 1: 
-            node->value = variant.GetLong();
-            return true;
-        default: 
-            wxLogError("GradientModel::SetValue wrong column %d", col);
-            return false;
+    if (auto container = dynamic_cast<GradientContainerNode*>(node)) {
+        switch (col) {
+            case 0:
+                container->name = variant.GetString();
+                return true;
+            case 1: /* do nothing */
+                return false;
+            default:
+                wxLogError("GradientModel::SetValue wrong column %d", col);
+                return false;
+        }
+    } else if (auto leaf = dynamic_cast<GradientEntityNode*>(node)) {
+        switch (col) {
+            case 0:
+                leaf->entity->title = variant.GetString();
+                return true;
+            case 1:
+                leaf->entity->value = variant.GetLong();
+                return true;
+            default:
+                wxLogError("GradientModel::SetValue wrong column %d", col);
+                return false;
+        }
+    } else {
+        wxLogError("GradientModel::GetValue unknown GradientModelNode");
     }
+    return false;
 }
 
 }
--- src/ui/gradient_model.hpp
+++ src/ui/gradient_model.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
+#include <map>
+#include <memory>
 #include <wx/dataview.h>
 
 namespace ui {
 
-struct GradientModelNode {
+struct Entity {
     std::string title;
     int value;
+};
+
+struct GradientModelNode {
+    GradientModelNode(GradientModelNode* parent_): parent(parent_){ }
+    GradientModelNode(): parent(NULL){ }
+    virtual ~GradientModelNode() = default;
+    virtual bool isContainer() const = 0;
+    GradientModelNode* parent;
+};
+
+struct GradientEntityNode : public GradientModelNode {
+    GradientEntityNode(GradientModelNode* parent, Entity* entity_): GradientModelNode(parent), entity(entity_) { }
+    virtual bool isContainer() const override { return false; }
+    Entity* entity;
+};
+
+struct GradientContainerNode: public GradientModelNode {
+    GradientContainerNode(GradientModelNode* parent, std::string name_): GradientModelNode(parent), name(name_) { }
+    std::string name;
+    std::vector<std::unique_ptr<GradientEntityNode>> children;
+    virtual bool isContainer() const override { return true; }
+    void addEntityChild(Entity* e) {
+        children.emplace_back(std::make_unique<GradientEntityNode>(this, e));
+    }
 };
 
 class GradientModel: public wxDataViewModel {
@@ -18,7 +44,8 @@ public:
     void GetValue(wxVariant& variant, const wxDataViewItem& item, unsigned col) const override;
     bool SetValue(const wxVariant& variant, const wxDataViewItem& item, unsigned col) override;
 private:
-    GradientModelNode modelNode;
+    std::map<std::string, std::vector<Entity>> mapping;
+    std::vector<std::unique_ptr<GradientContainerNode>> folders;
 };
 
 }
